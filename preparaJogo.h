274e23e52b7740e1578f38b902040d2df578522a
Luiz Carlos Ferreira <luizbionicle2647@gmail.com>
2021-06-04 23:25:58 -0300

    Remove header desnecessário e usa preparaJogo.h no lugar

--- preparaJogo.h
+++ preparaJogo.h
@@ -1,7 +1,39 @@
 #ifndef PREPARAJOGO_H
 #define PREPARAJOGO_H
 
-#include "comuns.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#define TAMANHO 11
+
+typedef struct
+{
+    int rodada;
+    int dificuldade;
+    int modoJogo;
+} Jogo;
+
+
+typedef struct
+{
+    bool humano;
+    char nome[30];
+    char titulo[20];
+    int pontuacao;
+    char jogadasRealizadas[1000][2];
+    char tabuleiro[TAMANHO][TAMANHO];
+    bool acertouAnterior;
+} Jogador;
+
+typedef struct
+{
+    char nome[30];
+    char classe;
+    int tamanho;
+    char posicao[5][2];
+} Navio;
 
 void imprimeTabuleiro(char tabuleiro[TAMANHO][TAMANHO]);
 
